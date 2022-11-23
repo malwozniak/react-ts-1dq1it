e825af4431e0e25392df993e5dcac1738dbcbb52
malwozniak <mgrztwnk5@gmail.com>
2022-11-23 09:06:26 +0100

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import AnimationList from './src/components/AnimationList';
+import Modal from './src/components/Modal';
+import AnimationCard from './src/components/AnimationCard';
+
+interface AppProps {}
+interface AppState {
+  modalVisible: boolean;
+  modalContent: any;
+  randomAnimation: object;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalVisible: false,
+      modalContent: '',
+      randomAnimation: {},
+    };
+
+    this.handleModalClose = this.handleModalClose.bind(this);
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  apiBasePath =
+    'https://raw.githubusercontent.com/malwozniak/react-ts-animation/main/animation.json';
+
+  async fetchRandomAnimation() {
+    const apiCall = await fetch(this.apiBasePath);
+
+    const data = await apiCall.json();
+    fetch(data)
+      .then((res) => res.json())
+      .then((dataa) => {
+        console.log('is working');
+      })
+      .catch((rejected) => {
+        console.log(rejected);
+      });
+    console.log(data);
+
+    this.setState((state, props) => {
+      return {
+        modalContent: <AnimationCard animation={data} />,
+        modalVisible: true,
+      };
+    });
+  }
+
+  generateRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  componentDidMount() {
+    this.fetchRandomAnimation();
+  }
+
+  handleItemClick(item: any) {
+    this.setState((state, props) => {
+      return {
+        modalContent: <AnimationCard animation={item} />,
+        modalVisible: true,
+      };
+    });
+  }
+
+  handleModalClose() {
+    this.setState((state, props) => {
+      return {
+        modalContent: '',
+        modalVisible: false,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.modalVisible && (
+          <Modal
+            content={this.state.modalContent}
+            onModalClose={this.handleModalClose}
+          />
+        )}
+        <AnimationList onItemClick={this.handleItemClick} />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
---
+++ src/components/AnimationCard.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Loader from 'react-loader-spinner';
+
+import AnimationProfile from './AnimationProfile';
+import AnimationTypes from './AnimationTypes';
+import { device } from '../../device';
+import { Animation } from '../types/Animation';
+
+type AnimationCardProps = {
+  Animation: Animation;
+};
+
+function AnimationCard({ Animation }: AnimationCardProps) {
+  const [AnimationDescription, setAnimationDescription] = useState<string>('');
+  const [isReady, setIsReady] = useState<boolean>(false);
+ 
+  useEffect(() => {
+    const fetchAnimationSpecies = async () => {
+      const result = await fetch(Animation.species.url);
+      return await result.json();
+    };
+
+    fetchAnimationSpecies().then((data) => {
+      const description = data.flavor_text_entries
+        .filter((item) => {
+          return item.language.name === 'en';
+        })[0]
+        .flavor_text.replace(/[^a-zA-Z é . , ']/g, ' ');
+      setAnimationDescription(description);
+      setIsReady(true);
+    });-
+  }, []);
+
+  return (
+    <AnimationCardContainer>
+      {isReady && (
+        <div>
+          <AnimationCardTitle>{Animation.name}</AnimationCardTitle>
+          <AnimationTypes types={Animation.types} />
+          <DoubleColumnCard>
+            <AnimationProfile
+              Animation={Animation}
+              description={AnimationDescription}
+            />
+          </DoubleColumnCard>
+        </div>
+      )}
+      {!isReady && (
+        <div className="d-flex justify-content-center mb-4">
+          <Loader
+            type="Puff"
+            color="#CCC"
+            height={100}
+            width={100}
+            timeout={3000}
+          />
+        </div>
+      )}
+    </AnimationCardContainer>
+  );
+}
+
+const AnimationCardContainer = styled.div`
+  font-size: 1.5em;
+  border-radius: 4px;
+`;
+
+const AnimationCardTitle = styled.h3`
+  text-transform: capitalize;
+  text-align: center;
+`;
+
+const DoubleColumnCard = styled.div`
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  align-items: center;
+
+  @media ${device.laptop} { 
+   flex-direction: row; 
+  }
+`;
+
+const AnimationDescription = styled.div`
+  font-size: 16px;
+  text-align: left;
+  margin-top: 20px;
+`;
+
+export default AnimationCard;
