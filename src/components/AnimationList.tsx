972aa67e173909941adfcd124c964fe6cfcbb49b
malwozniak <mgrztwnk5@gmail.com>
2022-11-30 09:43:50 +0100

    1 file modified

--- src/components/AnimationList.tsx
+++ src/components/AnimationList.tsx
@@ -1,87 +1,94 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
 
-import { Pokemon } from '../types/pokemon';
+import { Animation } from '../types/animation';
 
-type PokemonListProps = {
+type AnimationListProps = {
   onItemClick: (item: any) => void;
   scrollable: boolean;
 };
 
-type PokemonListState = {
-  pokemonData: Pokemon[],
-  nextUrl: string,
-  loading: boolean,
-  searchTerm: string
+type AnimationListState = {
+  AnimationData: Animation[];
+  nextUrl: string;
+  loading: boolean;
+  searchTerm: string;
 };
 
-class PokemonList extends React.Component<PokemonListProps, PokemonListState> {
+class AnimationList extends React.Component<
+  AnimationListProps,
+  AnimationListState
+> {
   constructor(props) {
     super(props);
     this.state = {
-      pokemonData: [],
-      nextUrl: "https://raw.githubusercontent.com/malwozniak/react-ts-1dq1it/main/animation.json",
+      AnimationData: [],
+      nextUrl:
+        'https://raw.githubusercontent.com/malwozniak/react-ts-1dq1it/main/animation.json',
       loading: false,
-      searchTerm: ''
+      searchTerm: '',
     };
 
     this.handleSearch = this.handleSearch.bind(this);
   }
 
-  getPokemonDataList() {
-    if(this.state.searchTerm != ''){
-      return this.state.pokemonData.filter((pokemon) => {
-        return pokemon.name.toLowerCase().indexOf(this.state.searchTerm.toLowerCase()) !== -1;
-      })
+  getAnimationDataList() {
+    if (this.state.searchTerm != '') {
+      return this.state.AnimationData.filter((animation) => {
+        return (
+          animation.name
+            .toLowerCase()
+            .indexOf(this.state.searchTerm.toLowerCase()) !== -1
+        );
+      });
     }
 
-    return this.state.pokemonData;
+    return this.state.AnimationData;
   }
 
   componentDidMount() {
     document.addEventListener('scroll', this.trackScrolling);
-    this.fetchPokemonListData();
+    this.fetchAnimationListData();
   }
 
-  fetchPokemonListData() {
+  fetchAnimationListData() {
     this.setState((state, props) => {
       return {
         loading: true,
-      }
+      };
     });
 
     setTimeout(() => {
       fetch(this.state.nextUrl)
-      .then(response => response.json())
-      .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           this.setState((state, props) => {
             return {
               nextUrl: data.next,
-            }
+            };
           });
 
           document.addEventListener('scroll', this.trackScrolling);
 
-          data.results.map(item => {
+          data.results.map((item) => {
             fetch(item.url)
-            .then(response => response.json())
-            .then(data => {
+              .then((response) => response.json())
+              .then((data) => {
                 this.setState((state, props) => {
-                  const pokemonData = [...this.state.pokemonData, data];
+                  const AnimationData = [...this.state.AnimationData, data];
                   return {
-                    pokemonData,
+                    AnimationData,
                     loading: false,
-                  }
-                }
-              );
-            });
-          })
-      });  
+                  };
+                });
+              });
+          });
+        });
     }, 1000);
   }
 
   handleSearch(event) {
-    this.setState({searchTerm: event.target.value});
+    this.setState({ searchTerm: event.target.value });
   }
 
   render() {
@@ -89,28 +96,30 @@ class PokemonList extends React.Component<PokemonListProps, PokemonListState> {
       <div className="container">
         <div className="row">
           <div className="my-4 p-0">
-            <input 
-              onChange={this.handleSearch} 
-              value={this.state.searchTerm} 
-              className="form-control" 
-              type="text" 
-              placeholder="Search" 
-              aria-label="Search" 
+            <input
+              onChange={this.handleSearch}
+              value={this.state.searchTerm}
+              className="form-control"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
             />
           </div>
 
-          {this.getPokemonDataList().map((item, index) => {
-              return (
-                <div onClick={(e) => this.handleItemClick(item, e) } className="col-sm-4 text-center text-capitalize card mb-4 list-item" key={item.name}>
-                  <h2>{item.name}</h2>
-                  <div> 
-                    <img src={item.sprites.animation_base} />
-                  </div>
+          {this.getAnimationDataList().map((item, index) => {
+            return (
+              <div
+                onClick={(e) => this.handleItemClick(item, e)}
+                className="col-sm-4 text-center text-capitalize card mb-4 list-item"
+                key={item.name}
+              >
+                <h2>{item.name}</h2>
+                <div>
+                  <img src={item.sprites.animation_base} />
                 </div>
-              )
+              </div>
+            );
           })}
-
-        
         </div>
       </div>
     );
@@ -132,9 +141,9 @@ class PokemonList extends React.Component<PokemonListProps, PokemonListState> {
     const wrappedElement = document.getElementsByClassName('container')[0];
     if (this.isBottom(wrappedElement) && this.props.scrollable === true) {
       document.removeEventListener('scroll', this.trackScrolling);
-      this.fetchPokemonListData();
+      this.fetchAnimationListData();
     }
   };
 }
 
-export default PokemonList;
+export default AnimationList;
