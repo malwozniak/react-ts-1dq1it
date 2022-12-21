be83b5243c7a2db124e86351ac762df691f073ef
malwozniak <mgrztwnk5@gmail.com>
2022-12-21 09:32:29 +0100

    1 file modified

--- src/components/animationsObjects/AnimationThreeD.tsx
+++ src/components/animationsObjects/AnimationThreeD.tsx
@@ -3,7 +3,13 @@ import { Canvas, useFrame } from '@react-three/fiber';
 
 function Box(props) {
   const mesh = useRef();
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  useFrame(
+    () => (
+      (mesh.current.rotation.y = mesh.current.rotation.x += 0.02),
+      (mesh.current.position.y += 0.003),
+      (mesh.current.position.y -= 0.004)
+    )
+  );
   return (
     <mesh {...props} ref={mesh}>
       <boxGeometry args={[3, 3, 3]} />
