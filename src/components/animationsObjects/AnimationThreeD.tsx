ab347483d30ada503c2bb9a599673b5959b3ed7f
malwozniak <mgrztwnk5@gmail.com>
2023-01-10 13:12:53 +0100

    1 file modified

--- src/components/animationsObjects/AnimationThreeD.tsx
+++ src/components/animationsObjects/AnimationThreeD.tsx
@@ -33,7 +33,7 @@ function Box(props) {
       <sphereGeometry attach="geometry" args={[1, 16, 16]} />
       <meshStandardMaterial
         attach="material"
-        color="white"
+        color="gray"
         transparent
         roughness={0.1}
         metalness={0.1}
