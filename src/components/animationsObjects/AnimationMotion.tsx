3aa76e1778d651a508ca2068eca782d3222239b3
malwozniak <mgrztwnk5@gmail.com>
2023-04-05 09:58:03 +0200

    Changed the main code in three Motion - up, down, left, right

--- src/components/animationsObjects/AnimationMotion.tsx
+++ src/components/animationsObjects/AnimationMotion.tsx
@@ -42,25 +42,8 @@ export default function AnimationMotion() {
 
   return (
     <div className="card-container">
-      {/* <div className="card square-card">
-        <div className="down">
-          <div className="up">
-            <div className="squeeze">
-              <div className="rotate-in">
-                <div className="rotate-out">
-                  <div className="square"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
       {/* <RandomImage num={generateRandomAnimation(1, 15)} /> */}
       {items[Math.round(Math.random() * 2)]}
-      {/* {items[Math.round(Math.random() * 1)]} */}
-      {/* <div className="card counter"></div>
-
-      <div className="card text-movement"></div> */}
     </div>
   );
 }
--- src/components/animationsObjects/AnimationThreeD.tsx
+++ src/components/animationsObjects/AnimationThreeD.tsx
@@ -1,27 +1,24 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { useFrame, useThree, useLoader } from '@react-three/fiber';
+import React, { useRef, useState, useEffect, useMemo } from 'react';
+import { useFrame, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 import { generateRandomAnimation } from '../../functions';
 
-let acceleration = 0.05;
-let bounce_distance = 2;
-let bottom_position_y = 0;
-let time_step = 0.1;
-let time_counter = Math.sqrt((-bounce_distance * 2) / -acceleration);
-let initial_speed = acceleration * time_counter;
-
-function Box(props) {
+function SphereMove(props) {
   const mesh = useRef();
   const { scene, gl } = useThree();
   const [isContextLost, setContextLost] = useState(false);
 
-  var num = Math.floor(Math.random() * 16) + 1;
-  let img =
-    'https://raw.githubusercontent.com/malwozniak/react-ts-1dq1it/main/textures/img' +
-    num +
-    '.jpg';
+  const acceleration = 0.05;
+  const bounce_distance = 2;
+  const bottom_position_y = 0;
+  const time_step = 0.1;
+  let time_counter = Math.sqrt((-bounce_distance * 2) / -acceleration);
+  const initial_speed = acceleration * time_counter;
 
-  const texture = useLoader(THREE.TextureLoader, img);
+  const num = useMemo(() => Math.floor(Math.random() * 16) + 1, []);
+  const img = useMemo(() => 'https://raw.githubusercontent.com/malwozniak/react-ts-1dq1it/main/textures/img' + num + '.jpg', [num]);
+
+  const texturesCache = useRef({});
 
   useEffect(() => {
     const onContextLost = (event) => {
@@ -32,52 +29,44 @@ function Box(props) {
     const onContextRestored = () => {
       setContextLost(false);
     };
-    console.log('image', img);
+
     gl.domElement.addEventListener('webglcontextlost', onContextLost, false);
-    gl.domElement.addEventListener(
-      'webglcontextrestored',
-      onContextRestored,
-      false
-    );
+    gl.domElement.addEventListener('webglcontextrestored', onContextRestored, false);
 
     return () => {
-      gl.domElement.removeEventListener(
-        'webglcontextlost',
-        onContextLost,
-        false
-      );
-      gl.domElement.removeEventListener(
-        'webglcontextrestored',
-        onContextRestored,
-        false
-      );
+      gl.domElement.removeEventListener('webglcontextlost', onContextLost, false);
+      gl.domElement.removeEventListener('webglcontextrestored', onContextRestored, false);
     };
   }, [gl]);
 
-  if (isContextLost) {
-    return null; // Return null when context is lost
-  }
+  useEffect(() => {
+    if (isContextLost) {
+      return;
+    }
 
-  scene.background = texture;
+    if (texturesCache.current[img]) {
+      scene.background = texturesCache.current[img];
+    } else {
+      const loader = new THREE.TextureLoader();
+      loader.load(img, (texture) => {
+        texturesCache.current[img] = texture;
+        scene.background = texture;
+      });
+    }
+  }, [img, isContextLost, scene]);
 
   useFrame(() => {
     if (num % 2) {
       if (mesh.current.position.x < bottom_position_y) {
         time_counter = 0;
       }
-      mesh.current.position.x =
-        bottom_position_y +
-        initial_speed * time_counter -
-        0.5 * acceleration * time_counter * time_counter;
+      mesh.current.position.x = bottom_position_y + initial_speed * time_counter - 0.5 * acceleration * time_counter * time_counter;
       time_counter += time_step;
     } else {
       if (mesh.current.position.y < bottom_position_y) {
         time_counter = 0;
       }
-      mesh.current.position.y =
-        bottom_position_y +
-        initial_speed * time_counter -
-        0.5 * acceleration * time_counter * time_counter;
+      mesh.current.position.y = bottom_position_y + initial_speed * time_counter - 0.5 * acceleration * time_counter * time_counter;
       time_counter += time_step;
     }
   });
@@ -85,15 +74,9 @@ function Box(props) {
   return (
     <mesh {...props} ref={mesh}>
       <sphereGeometry attach="geometry" args={[1, 16, 16]} />
-      <meshStandardMaterial
-        attach="material"
-        color="gray"
-        transparent
-        roughness={0.1}
-        metalness={0.1}
-      />
+      <meshStandardMaterial attach="material" color="gray" transparent roughness={0.1} metalness={0.1} />
     </mesh>
-  );
+  )
 }
 
-export default Box;
+export default SphereMove;
