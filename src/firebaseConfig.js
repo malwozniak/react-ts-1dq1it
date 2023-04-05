e47d311296c047a59823787c7130b8a0652f2499
malwozniak <mgrztwnk5@gmail.com>
2023-04-05 13:11:41 +0200

    1 file modified

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,8 +2,15 @@ export const firebaseConfig = {
   apiKey: 'YOUR_API_KEY',
   authDomain: 'YOUR_AUTH_DOMAIN',
   databaseURL: 'YOUR_DATABASE_URL',
-  projectId: 'YOUR_PROJECT_ID',
+  projectId: 'animationeffective',
   storageBucket: 'YOUR_STORAGE_BUCKET',
   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
   appId: 'YOUR_APP_ID',
 };
+
+
+/**
+
+Po utworzeniu projektu, chciałam utworzyć bazę danych, następnie wybrałam "Start in test mode", aby utworzyć nową bazę danych w trybie testowym.
+Wybierałam lokalizację dla swojej bazy danych (zaleca się wybrać lokalizację najbliżej użytkowników, aby uzyskać lepszą wydajność).
+
